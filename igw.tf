35102d9ad1675f92bbf6e2c019e1141d85276ce9
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2021-01-24 18:43:37 -0500

    Update igw.tf

--- igw.tf
+++ igw.tf
@@ -1,9 +1,11 @@
-resource "aws_internet_gateway" "test" {
-  vpc_id = "${aws_vpc.myvpc.id}"
+resource aws_internet_gateway nicrogg {
+  vpc_id = aws_vpc.myvpc.id
   depends_on = [
     aws_vpc.myvpc,
   ]
   tags = {
-    Name = "test"
+    Name = "nicrogg"
   }
 }
+
+
