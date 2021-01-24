ce42eff3eb3d382b4b681e58adcde571af75359a
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2021-01-24 18:33:44 -0500

    Update MysqlLaunchec2.tf

--- MysqlLaunchec2.tf
+++ MysqlLaunchec2.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "mysqlOS" {
   ami           = "ami-03130878b60947df3"
   instance_type = "t2.micro"
   key_name      = "Caf-Nkey"
-  subnet_id =  aws_subnet.prisub.id
-  vpc_security_group_ids = [ "aws_security_group.SG.id" ]
+  availability_zone = "us-west-1b"
+  subnet_id =  aws_subnet.privatesub.id
+  vpc_security_group_ids = [ aws_security_group.mysqlsg.id ]
   tags = {
     Name = "mysqlOS"
   }
