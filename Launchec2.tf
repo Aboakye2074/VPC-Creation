a455af096f65ebb23d5b5a4cacd0be09a4df0816
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2021-01-24 18:31:42 -0500

    Update Launchec2.tf

--- Launchec2.tf
+++ Launchec2.tf
@@ -1,12 +1,14 @@
 resource "aws_instance" "wordpressOS" {
   ami           = "ami-03130878b60947df3"
-  instance_type = "t2.micro" 
+  instance_type = "t2.micro"
 associate_public_ip_address = true
   key_name      = "Caf-Nkey"
-  subnet_id =  aws_subnet.pubsub.id
-  vpc_security_group_ids = [ aws_security_group.SG.id ]
+  subnet_id =  aws_subnet.publicsub.id
+  availability_zone = "us-west-1a"
+  vpc_security_group_ids = [ aws_security_group.task4.id ]
   tags = {
     Name = "wordpressOS"
-  
+
   }
 }
+
