d689c42fd0a0ad9bd4d712207041c88ef6c9a575
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2021-01-24 18:27:53 -0500

    Update AssoSub1.tf

--- AssoSub1.tf
+++ AssoSub1.tf
@@ -1,7 +1,8 @@
 resource "aws_route_table_association" "associate_1" {
-  subnet_id      = aws_subnet.pubsub.id
-  route_table_id = aws_route_table.testtab1.id
+  subnet_id      = aws_subnet.publicsub.id
+  route_table_id = aws_route_table.nicrotab1.id
   depends_on = [
     aws_subnet.publicsub,
   ]
 }
+
