1e2443d13b7bb87eead1da8c367d79e14008536d
Aboakye2074 <70584084+Aboakye2074@users.noreply.github.com>
2021-01-24 18:53:17 -0500

    Update routetable.tf

--- routetable.tf
+++ routetable.tf
@@ -1,14 +1,14 @@
-resource "aws_route_table" "testtab1" {
+resource "aws_route_table" "nicrotab1" {
   vpc_id = aws_vpc.myvpc.id
- 
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.test.id
+    gateway_id = aws_internet_gateway.nicrogg.id
   }
   depends_on = [
     aws_vpc.myvpc,
   ]
   tags = {
-    Name = "test"
+    Name = "nicro"
   }
 }
